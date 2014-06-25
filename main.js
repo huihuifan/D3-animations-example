0091342827631a8ea120417419e98839a65c3192
huihuifan <anarfish@gmail.com>
2014-06-25 11:50:31 -0400

    added transitioning line graph, set up for generalized number of time slices, with update function

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 //Angela Fan
 
+var file_list = ["time1.csv", "time2.csv"];
+
+
 var margins = [50, 50, 50, 50],
 	width = 1060,
-	h = 500;
-
+	height = 500;
 
 var x, y;
 
 var duration = 1200, 
 	delay = 500;
-
 
 var color = d3.scale.category10();
 
@@ -17,7 +18,7 @@ var svg = d3.select("#graphs").append("svg")
 	.attr("width", width + margins[1] + margins[3])
 	.attr("height", height + margins[0] + margins[2])
   .append("g")
-  	.attr("transform", "translate(" + margins[3] + "," + margins[0]);
+  	.attr("transform", "translate(" + margins[3] + "," + margins[0] + ")");
 
 var x = d3.scale.linear()
 	.range([0, width]);
@@ -52,7 +53,9 @@ d3.csv("time1.csv", function(error, data) {
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis)
 	  .append("text")
-	  	.attr("x", 10)
+	  	.attr("x", 1100)
+	  	.attr("dx", "-3.0em")
+	  	.attr("dy", "2.9em")
 	  	.style("text-anchor", "end")
 	  	.text("Location");
 
@@ -70,13 +73,42 @@ d3.csv("time1.csv", function(error, data) {
 		.datum(data)
 		.attr("class", "line")
 		.attr("d", line);
-})
 
+	var num_files = file_list.length;
 
+	for (var i = 0; i < num_files; i ++) {	
+		setTimeout(update(file_list[i]), 2000);
+	}
 
+});
 
+function update(file){
 
+  	d3.csv(file, function(error, data) {
+  		data.forEach(function(d) {
+		d.location = +d.location;
+		d.signal = +d.signal;
+	});
+  	
+  	x.domain(d3.extent(data, function(d) {return d.location}));
+	y.domain(d3.extent(data, function(d) {return d.signal}));
 
+	svg.select(".x").call(xAxis);
+	svg.select(".y").call(yAxis);
+
+	var selection = svg.selectAll(".line")
+		.datum(data);
+
+	selection
+		.transition()
+		.duration(1000)
+		.attr("d", line)
+		.ease("linear")
+		.attr("class", "line");
+
+	});
+
+}
 
 
 
