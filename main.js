b7510ebf227e0e11cabd79bd7f71c408c178a541
huihuifan <anarfish@gmail.com>
2014-06-26 17:47:59 -0400

    now working with everything, etc. stuff

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 //Angela Fan
 
-var file_list = ["time1.csv", "time2.csv", "time3.csv", "time4.csv", "time5.csv"];
-
+var number = 4;
 
 var margins = [50, 50, 50, 50],
 	width = 1060,
 	height = 500;
-
-var x, y;
-
-var duration = 1200, 
-	delay = 500;
-
-var color = d3.scale.category10();
 
 var svg = d3.select("#graphs").append("svg")
 	.attr("width", width + margins[1] + margins[3])
@@ -21,30 +13,30 @@ var svg = d3.select("#graphs").append("svg")
   	.attr("transform", "translate(" + margins[3] + "," + margins[0] + ")");
 
 var x = d3.scale.linear()
-	.range([0, width]);
+    .range([0, width]);
 
 var y = d3.scale.linear()
-	.range([height, 0]);
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 var line = d3.svg.line()
 	.interpolate("basis")
-	.x(function(d) {return x(d.location)})
-	.y(function(d) {return y(d.signal)});
+	.x(function(d) { return x(d.location) })
+	.y(function(d) { return y(d.signal) });
 
-var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient("bottom");
-
-var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient("left");
-
-d3.csv("time1.csv", function(error, data) {
+d3.csv("ec_brd4.csv", function(error, data) {
 	data.forEach(function(d) {
 		d.location = +d.location;
-		d.signal = +d.signal;
+		d.signal = +d.signal1;
 	});
-
+	
 	x.domain(d3.extent(data, function(d) {return d.location}));
 	y.domain(d3.extent(data, function(d) {return d.signal}));
 
@@ -74,36 +66,26 @@ d3.csv("time1.csv", function(error, data) {
 		.attr("class", "line")
 		.attr("d", line);
 
-	var num_files = file_list.length;
-
-	// for (i = 0; i < num_files; ++i) {
- //  		setTimeout(setDelay(i), 1000);
-	// }
-
-	// function setDelay(i) {
-	//   setTimeout(function(){
-	//     update(file_list[i]);
-	//   }, 1000);
-	// }
-
 	function setDelay() {
-	    var file = file_list.shift();
-	    if (file) {
-	        update(file);
-	        setTimeout(setDelay, 2000);
-	    }
+		number = number - 1;
+		if (number) {
+			update(number)
+			setTimeout(setDelay, 3000);
+		}
 	}
 
 	setDelay();
 
 });
 
-function update(file){
+function update(number){
 
-  	d3.csv(file, function(error, data) {
+	var col_name = "signal" + number;
+
+  	d3.csv("ec_brd4.csv", function(error, data) {
   		data.forEach(function(d) {
 		d.location = +d.location;
-		d.signal = +d.signal;
+		d.signal = +d[col_name];
 	});
   	
   	x.domain(d3.extent(data, function(d) {return d.location}));
